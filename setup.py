0023b667349c66bf53530262ad6078f0180b5e85
Chocks <chocku.engr@gmail.com>
2025-04-12 14:39:09 +0000

    initial commit

--- setup.py
+++ setup.py
@@ -2,14 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydatamorph',
-    version='0.2',
+    version='1.0.0',
     packages=find_packages(),
-    install_requires=[],
+    include_package_data=True,
+    install_requires=[
+        "streamlit>=1.0"
+    ],
     author='Chockalingam Subramanian',
     description='Adaptive LLM-native data pipeline orchestrator',
     entry_points={
         'console_scripts': [
             'datamorph=datamorph.core.runner:main'
         ]
-    }
+    },
+    python_requires='>=3.7',
 )
