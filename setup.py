00dad00ef4a71d3ffd2b73d62ae7f9e4bb5a55be
Chocks <chocku.engr@gmail.com>
2025-04-12 17:05:24 +0000

    initial commit

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydatamorph',
-    version='1.0.8',
-    packages=find_packages(),
+    version='1.0.9',
+    #packages=find_packages(),
+    packages=['datamorph','datamorph.core','datamorph.ui','datamorph.config','datamorph.llm','datamorph.steps','datamorph.utils'],
     include_package_data=True,
     install_requires=[
         "streamlit>=1.0",
